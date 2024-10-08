8470ec850496f097d772040db5f9912d4f51d150
Linus Johansson <linusjohaan@gmail.com>
2024-10-08 14:59:49 +0200

    small changes in the printout

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿using System.Diagnostics;
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
 using System.Reflection.Metadata;
+using System.Security.Cryptography;
 
 static class Program
 {
@@ -14,7 +17,7 @@ static class Program
         NyUtryckning.Add(new Utryckning("Rån","Göteborg","Kungsgatan",DateTime.Parse("2021-10-10 12:00"),new List<Personal>{Konstapel[0],Konstapel[1]}));
         NyUtryckning.Add(new Utryckning("Kollision","Göteborg","Kungsgatan",DateTime.Parse("2021-10-10 12:00"),new List<Personal>{Konstapel[0],Konstapel[1]}));
         NyUtryckning.Add(new Utryckning("Rån","Växjö","Centrum",DateTime.Parse("2023-02-15 16:10"),new List<Personal>{Konstapel[0],Konstapel[3]}));
-        NyUtryckning.Add(new Utryckning("Mord","Stockholm","Drottninggatan",DateTime.Parse("2021-10-11 01:00"),new List<Personal>{Konstapel[2],Konstapel[3]}));
+        NyUtryckning.Add(new Utryckning("Mord","Stockholm","Drottninggatan",DateTime.Parse("2021-10-11 01:00"),new List<Personal>{Konstapel[2],Konstapel[3], Konstapel[0]}));
 
         PrintMenu();
     }
@@ -73,27 +76,38 @@ static class Program
             PrintPersonal();
             Console.Write("Index: ");
             int index=int.Parse(Console.ReadLine());
+            
             List<Personal>PolisPåplats=new();
             //PolisPåplats.Add(Konstapel[index]);
             NyUtryckning.Add(new Utryckning(händelsetyp,stad,plats,tidpunkt,PolisPåplats));
             Console.Clear();
+            Console.BackgroundColor= ConsoleColor.Green;
+            Console.WriteLine("Ny Utryckning tillagd");
+            Console.ResetColor();
             PrintMenu();       
     }
     public static void AddPersonal()
     {
         Console.Write("Skriv in För och efternamn: ");
         string namn=Console.ReadLine();
+        
         Console.Write("Ange Tjänstenummer: ");
         int tjänstNR=int.Parse(Console.ReadLine());
-        if(tjänstNR==tjänstNR)
+        if(Konstapel.Any(p => p.tjänstNR==tjänstNR))
         {
+            Console.BackgroundColor=ConsoleColor.Red;
             Console.WriteLine("Tjänstenummer finns redan");
+            Console.ResetColor();
+            AddPersonal();
         }
         else
         {
-        Konstapel.Add(new Personal(namn, tjänstNR));
+            Console.Clear();
+            Konstapel.Add(new Personal(namn, tjänstNR));
+            Console.BackgroundColor= ConsoleColor.Green;
+            Console.WriteLine("Ny personal tillagd");
+            Console.ResetColor();
         }
-        Console.Clear();
     }
     public static void AddRapport()
     {
@@ -104,6 +118,9 @@ static class Program
         string rapport = Console.ReadLine();
         NyUtryckning[index].rapport=rapport;
         Console.Clear();
+        Console.BackgroundColor= ConsoleColor.Green;
+        Console.WriteLine("Ny Rapport tillagd");
+        Console.ResetColor();
        
     }
     public static void PrintPersonal()
@@ -121,10 +138,14 @@ static class Program
             Console.Clear();
             for (int i=0;i<NyUtryckning.Count;i++)
             {
-                Console.WriteLine($"{i}||{NyUtryckning[i].händelsetyp}||{NyUtryckning[i].stad}||{NyUtryckning[i].tidpunkt}||{NyUtryckning[i].rapport}");
+                Console.ForegroundColor=ConsoleColor.DarkYellow;
+                Console.WriteLine($"[{i}]");
+                Console.ResetColor();
+                Console.WriteLine($"||{NyUtryckning[i].händelsetyp}||Station {NyUtryckning[i].stad}||{NyUtryckning[i].tidpunkt}||{NyUtryckning[i].rapport}");
+                Console.WriteLine("   ===Polis på Plats===");
                 for (int p=0;p<NyUtryckning[i].PolisPåplats.Count;p++)
                 {
-                    Console.WriteLine($"Polis på Plats\n||{NyUtryckning[i].PolisPåplats[p].namn}");
+                    Console.WriteLine($"||{NyUtryckning[i].PolisPåplats[p].namn} || {NyUtryckning[i].PolisPåplats[p].tjänstNR}");
                 }
             }
         //}
@@ -135,44 +156,47 @@ static class Program
         Console.WriteLine("================Rapporter================");
         for (int i=0;i<NyUtryckning.Count;i++)
             {
+                Console.WriteLine("===================================");
                 Console.WriteLine($"Den {NyUtryckning[i].tidpunkt} skedde det en utryckning för {NyUtryckning[i].händelsetyp} i {NyUtryckning[i].stad}\nBeskrivningen av händelsen:{NyUtryckning[i].rapport}");
                 Console.WriteLine($"Utryckande Personal från Station: {NyUtryckning[i].stad}");
-                Console.WriteLine("Polis som kom till platsen var");
+                Console.WriteLine("|");
+                Console.WriteLine("Polis som kom till platsen var följande:");
                 for (int j=0;j<NyUtryckning[i].PolisPåplats.Count;j++)
                 {
                     Console.WriteLine($"{NyUtryckning[i].PolisPåplats[j].namn}||{NyUtryckning[i].PolisPåplats[j].tjänstNR}");
+                    
                 }
             }
         Console.WriteLine("==============Rapporter Slut===============");
     }
-    public class Utryckning
+}
+public class Utryckning
+{
+    public string händelsetyp;
+    public string stad;
+    public string plats;
+    public DateTime tidpunkt;
+    public string rapport;
+    public List<Personal>PolisPåplats;
+
+    public Utryckning(string händelsetyp,string stad, string plats, DateTime tidpunkt,List<Personal>PolisPåplats)
     {
-        public string händelsetyp;
-        public string stad;
-        public string plats;
-        public DateTime tidpunkt;
-        public string rapport;
-        public List<Personal>PolisPåplats;
+        this.händelsetyp=händelsetyp;
+        this.stad=stad;
+        this.plats=plats;
+        this.tidpunkt=tidpunkt;
+        this.PolisPåplats=PolisPåplats;
 
-        public Utryckning(string händelsetyp,string stad, string plats, DateTime tidpunkt,List<Personal>PolisPåplats)
-        {
-            this.händelsetyp=händelsetyp;
-            this.stad=stad;
-            this.plats=plats;
-            this.tidpunkt=tidpunkt;
-            this.PolisPåplats=PolisPåplats;
-
-        }
     }
-    public class Personal
-    {
-        public string namn;
-        public int tjänstNR;
+}
+public class Personal
+{
+    public string namn;
+    public int tjänstNR;
 
-        public Personal(string namn, int tjänstNR)
-        {
-            this.namn=namn;
-            this.tjänstNR=tjänstNR;
-        }
+    public Personal(string namn, int tjänstNR)
+    {
+        this.namn=namn;
+        this.tjänstNR=tjänstNR;
     }
 }
